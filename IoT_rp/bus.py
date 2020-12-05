49284691897a8c3248f28acc20a102cbf2dab875
h20190099 <60843461+h20190099@users.noreply.github.com>
2020-12-05 16:18:45 +0530

    Update bus.py

--- IoT_rp/bus.py
+++ IoT_rp/bus.py
@@ -1,5 +1,4 @@
 # importing libraries
-import boto3
 from csv import reader
 import sys
 import paho.mqtt.client as paho
@@ -13,15 +12,14 @@ from array import *
 import time
 
 global check
-global county
+global count
 global n
 global sec
 
 
 
 #RFID = [1,2,3,4,5,6,7,8,9,10]             #registered valid RFID card numbers
-bus = [201,202,203,204]                    #Bus numbers
-#new=[]                                    #will contain valid RFID card holders present in the bus
+bus = [201,202,203,204,205,206]            #Bus numbers
 route1 =[]
 route2 =[]
 route3 =[]
@@ -35,12 +33,9 @@ stops =[]
 
 max_count=10                              #maximum bus capacity
 opt_count=5                               #optimum bus capacity
-#county = 0
-#exe = 0
+count = 0
 
 
-
- 
 connflag = False
  
 def on_connect(client, userdata, flags, rc):                # func for making connection
@@ -79,62 +74,21 @@ mqttc.loop_start()                                          # Start the loop
 while 1:
     sleep(5)
     if connflag == True:
-        with open('rp_add.csv', 'r') as read_obj:
+        with open('rp_up.csv', 'r') as read_obj:
             csv_reader = reader(read_obj)
+            
         
             # Pass reader object to list() to get a list of lists
             list_of_rows = list(csv_reader)
+            lines= len(list_of_rows)
+            for k in range(1,lines):
+                route.append(list_of_rows[k][0])
+                stops.append(list_of_rows[k].index('D'))
             
-            for element in list_of_rows[1]:
-                if element != '':
-                    route1.append(element)
-            for element in list_of_rows[2]:
-                if element != '':
-                    route2.append(element)
-            for element in list_of_rows[3]:
-                if element != '':
-                    route3.append(element)
-            for element in list_of_rows[4]:
-                if element != '':
-                    route4.append(element)
-            for element in list_of_rows[5]:
-                if element != '':
-                    route5.append(element)
-            for element in list_of_rows[6]:
-                if element != '':
-                    route6.append(element)        
+            print ("\nRoutes:",route)
+            print ("\nStops in respective routes:",stops)
+                
 
-
-
-
-
-                    
-            print (route1)
-            print(len(route1)-1)
-            print (route2)
-            print(len(route2)-1)
-            print (route3)
-            print(len(route3)-1)
-            print (route4)
-            print(len(route4)-1)
-            print (route5)
-            print(len(route5)-1)
-            print (route6)
-            print(len(route6)-1)
-            route.append(route1[0])
-            route.append(route2[0])
-            route.append(route3[0])
-            route.append(route4[0])
-            route.append(route5[0])
-            route.append(route6[0])
-            print (route)
-            stops.append(len(route1)-1)
-            stops.append(len(route2)-1)
-            stops.append(len(route3)-1)
-            stops.append(len(route4)-1)
-            stops.append(len(route5)-1)
-            stops.append(len(route6)-1)
-            print (stops)
             
         key ='lCjtPeawYr7eGGxB5g1qTclW9lCHaTfC'
         url ='http://www.mapquestapi.com/geocoding/v1/address?key='
@@ -143,19 +97,15 @@ while 1:
         loc3 = 'Sasoon Dock'
         loc4 = 'Colaba Depot'
         loc5 = 'Mantralaya'
-        loc6 = 'Dhobi Ghat Colaba'
-        loc7 = 'Strand Cinema'
-        loc8 = 'Hutatma Chowk'
+        
+        
         
         main_url_1= url + key +'&location=' + loc1
         main_url_2= url + key +'&location=' + loc2
         main_url_3= url + key +'&location=' + loc3
         main_url_4= url + key +'&location=' + loc4
         main_url_5= url + key +'&location=' + loc5
-        main_url_6= url + key +'&location=' + loc6
-        main_url_7= url + key +'&location=' + loc7
-        main_url_8= url + key +'&location=' + loc8
-
+       
         r1 = requests.get(main_url_1)
         data = r1.json()['results'][0]
         location1 =data['locations'][0]
@@ -188,50 +138,27 @@ while 1:
         lon5 = location5['latLng']['lng']
 
 
-        r6 = requests.get(main_url_6)
-        data6 = r6.json()['results'][0]
-        location6 =data6['locations'][0]
-        lat6 = location6['latLng']['lat']
-        lon6 = location6['latLng']['lng']
-
-
-        r7 = requests.get(main_url_7)
-        data7 = r7.json()['results'][0]
-        location7 =data7['locations'][0]
-        lat7 = location7['latLng']['lat']
-        lon7 = location7['latLng']['lng']
-
-        r8 = requests.get(main_url_8)
-        data8 = r8.json()['results'][0]
-        location8 =data8['locations'][0]
-        lat8 = location8['latLng']['lat']
-        lon8 = location8['latLng']['lng']
-
         latitude.append(lat1)
         latitude.append(lat2)
         latitude.append(lat3)
         latitude.append(lat4)
         latitude.append(lat5)
-        latitude.append(lat6)
-        latitude.append(lat7)
-        latitude.append(lat8)
-        print (latitude)
+       
+        print ("\nLatitude :",latitude)
 
         longitude.append(lon1)
         longitude.append(lon2)
         longitude.append(lon3)
         longitude.append(lon4)
         longitude.append(lon5)
-        longitude.append(lon6)
-        longitude.append(lon7)
-        longitude.append(lon8)
-        print (longitude)
+        
+        print ("\nLongitude :",longitude)
 
 
         # define the countdown func. 
         def countdown(t,n,sec):
             
-            print('Bus registration number', str(sys.argv[1]))
+            print('\nBus registration number', str(sys.argv[1]))
             
             route_max = len(route)
             Total_pass = 0
@@ -245,8 +172,7 @@ while 1:
                     pas_count = 0 
 
                 Total_pass = Total_pass + pas_count
-                lat=latitude[i-1]
-                lon=longitude[i-1]
+                
                     
                 now = datetime.now()
                 current_time = now.strftime("%H:%M:%S")
@@ -260,7 +186,11 @@ while 1:
                     
                      
                 
-                print('At Bustop :',i)
+                print('\nAt Bustop :',i)
+                
+                lat= random.choice(latitude)
+                lon= random.choice(longitude)
+                
                 print('latitude :',lat)
                 print('longitude :',lon)
                 print('Number of passenger:',pas_count)
@@ -283,7 +213,7 @@ while 1:
                 
                 
                 if pas_count > opt_count and pas_count < max_count:
-                    print(" Bus is over loaded")
+                    print("Bus is over loaded")
                 elif pas_count < opt_count and  pas_count >0 :
                     print("Bus is under loaded")
                 elif pas_count == opt_count:
@@ -306,7 +236,7 @@ while 1:
         length = len(route)
         for j in range( 0,length):
             if str(sys.argv[2])== str(route[j]):
-                print('\n\nRoute scheduled:',j+1)
+                print('\n\nRoute scheduled:',int(route[j]))
                 print('Total stops in scheduled route:',stops[j])
                 countdown(int(10),stops[j],0)
                
